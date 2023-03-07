27cbc3da756be5f9d76767e2d9b32ffe1f9649b1
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-07 20:32:52 +0530

    Added: program to find the maximum in strings

--- src/main/java/com/bridgelabz/generics/MaxUsingGenerics.java
+++ src/main/java/com/bridgelabz/generics/MaxUsingGenerics.java
@@ -22,12 +22,24 @@ public class MaxUsingGenerics {
         }
         return maximum;
     }
+    public String getMaxString(String g, String h, String i){
+        String maximum = g;
+        if(h.compareTo(g) >0){
+            maximum = h;
+        }
+        if(i.compareTo(h) > 0){
+            maximum = i;
+        }
+        return maximum;
+    }
 
     public static void main(String[] args) {
         Integer a = 10, b = 60, c = 20;
         System.out.println("Maximum Integer is " + new MaxUsingGenerics().getMaxInteger(a, b, c));
         Float d = 50.6f, e = 30.6f, f = 94.9f;
         System.out.println("Maximum Float is " + new MaxUsingGenerics().getMaxFloat(d, e, f));
+        String g = "Apple", h = "Peach", i = "Banana";
+        System.out.println("Maximum String is " + new MaxUsingGenerics().getMaxString(g, h, i));
     }
 }
 
