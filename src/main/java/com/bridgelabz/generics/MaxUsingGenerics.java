682ab401279840be1211e7d5bf664535114ebe2c
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-07 21:35:03 +0530

    Refactored: Program to find the maximum using generic class

--- src/main/java/com/bridgelabz/generics/MaxUsingGenerics.java
+++ src/main/java/com/bridgelabz/generics/MaxUsingGenerics.java
@@ -1,6 +1,14 @@
 package com.bridgelabz.generics;
 
-public class MaxUsingGenerics {
+public class MaxUsingGenerics<T extends Comparable> {
+    T a, b, c;
+
+    public MaxUsingGenerics(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
     public <E extends Comparable> E getMaximum(E a, E b, E c) {
         E maximum = a;
         if (b.compareTo(a) > 0) {
@@ -14,11 +22,11 @@ public class MaxUsingGenerics {
 
     public static void main(String[] args) {
         Integer a = 10, b = 60, c = 20;
-        System.out.println("Maximum Integer is " + new MaxUsingGenerics().getMaximum(a, b, c));
+        System.out.println("Maximum Integer is " + new MaxUsingGenerics(a, b, c).getMaximum(a, b, c));
         Float d = 50.6f, e = 30.6f, f = 94.9f;
-        System.out.println("Maximum Float is " + new MaxUsingGenerics().getMaximum(d, e, f));
+        System.out.println("Maximum Float is " + new MaxUsingGenerics(d, e, f).getMaximum(d, e, f));
         String g = "Apple", h = "Peach", i = "Banana";
-        System.out.println("Maximum String is " + new MaxUsingGenerics().getMaximum(g, h, i));
+        System.out.println("Maximum String is " + new MaxUsingGenerics(g, h, i).getMaximum(g, h, i));
     }
 }
 
