585001ce809de29225e19d14d80e787f724b5808
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-07 21:27:51 +0530

    Refactored: program to find the maximum using single method

--- src/main/java/com/bridgelabz/generics/MaxUsingGenerics.java
+++ src/main/java/com/bridgelabz/generics/MaxUsingGenerics.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.generics;
 
 public class MaxUsingGenerics {
-    public Integer getMaxInteger(Integer a, Integer b, Integer c) {
-        Integer maximum = a;
+    public <E extends Comparable> E getMaximum(E a, E b, E c) {
+        E maximum = a;
         if (b.compareTo(a) > 0) {
             maximum = b;
         }
@@ -12,34 +12,13 @@ public class MaxUsingGenerics {
         return maximum;
     }
 
-    public Float getMaxFloat(Float d, Float e, Float f) {
-        Float maximum = d;
-        if (e.compareTo(d) > 0) {
-            maximum = e;
-        }
-        if(f.compareTo(e) > 0){
-            maximum = f;
-        }
-        return maximum;
-    }
-    public String getMaxString(String g, String h, String i){
-        String maximum = g;
-        if(h.compareTo(g) >0){
-            maximum = h;
-        }
-        if(i.compareTo(h) > 0){
-            maximum = i;
-        }
-        return maximum;
-    }
-
     public static void main(String[] args) {
         Integer a = 10, b = 60, c = 20;
-        System.out.println("Maximum Integer is " + new MaxUsingGenerics().getMaxInteger(a, b, c));
+        System.out.println("Maximum Integer is " + new MaxUsingGenerics().getMaximum(a, b, c));
         Float d = 50.6f, e = 30.6f, f = 94.9f;
-        System.out.println("Maximum Float is " + new MaxUsingGenerics().getMaxFloat(d, e, f));
+        System.out.println("Maximum Float is " + new MaxUsingGenerics().getMaximum(d, e, f));
         String g = "Apple", h = "Peach", i = "Banana";
-        System.out.println("Maximum String is " + new MaxUsingGenerics().getMaxString(g, h, i));
+        System.out.println("Maximum String is " + new MaxUsingGenerics().getMaximum(g, h, i));
     }
 }
 
